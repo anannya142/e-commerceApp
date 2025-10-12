3216da122af542303da4552edf104a40dda3fa00
anannya142 <anannyatanwee142@gmail.com>
2025-10-12 20:46:17 +0200

    fix admin login page

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -40,7 +40,7 @@ const Login = ({setToken}) => {
                     
                 </div>
                  <div className='mb-3 min-w-72'>
-                    <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                    <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
                     <input onChange ={ (e)=>setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none'type="password" placeholder='Enter your password' required/>
                     
                 </div>
